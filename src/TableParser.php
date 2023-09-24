ad3d83d334b8c6d4576865148ee5c08871d26e78
mengx <mxzls@qq.com>
2023-09-24 20:10:56 +0800

    添加 timestamp 里的允许为空的变更

--- src/TableParser.php
+++ src/TableParser.php
@@ -184,7 +184,7 @@ class TableParser
             $column = $table->float($columnName,intval($total),intval($places),$default)->setColumnName($typeContent);
         }elseif($typeContent === 'timestamp'){
             $defaultCurrent = $default === 'CURRENT_TIMESTAMP';
-            $column = $table->timestamp($columnName,$defaultCurrent);
+            $column = $table->timestamp($columnName,$defaultCurrent)->nullable($default);
         }elseif($typeContent === 'json'){
             $column = $table->json($columnName)->setColumnName($typeContent);
         }
